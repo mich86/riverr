88b54abae6fed3428e3a09adc66c25114538f487
Michelle Campbell <Michelle@Michelles-MacBook-Pro.local>
2022-07-20 20:48:56 +0100

    tailwind setup, config files, FE script- build is watching and running

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// @import "" should always be at the top of the file
