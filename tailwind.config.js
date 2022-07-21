fa1d807776171d2f40c84fc431b26a010fd25324
Michelle Campbell <Michelle@Michelles-MacBook-Pro.local>
2022-07-21 10:06:11 +0100

    tailwind config updates with colours, fonts, screens. Amends to index semantics

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,35 @@ module.exports = {
     './public/**/*.{html,js}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '320px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px',
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'black': '#000000',
+        'white': '#ffffff',
+        'red': '#881024',
+        'grey': {
+          100: '#6c757d',
+          500: '#f1f1f1',
+          900: '#656565',
+        },
+      },
+      fontSize: {
+        xs: ['0.5rem', { lineHeight: '1.5rem' }], //8
+        sm: ['0.75rem', { lineHeight: '1.5rem' }], //12
+        md: ['0.938rem', { lineHeight: '1rem' }], //15
+        base: ['1rem', { lineHeight: '1.5rem' }], //16
+        lg: ['1.125rem', { lineHeight: '1.25rem' }], //18
+        xl: ['1.375rem', { lineHeight: '1.75rem' }], //22
+        '2xl': ['1.75rem', { lineHeight: '2rem' }], //28
+      },
+    },
   },
   plugins: [],
 }
-
-// @import "" should always be at the top of the file
